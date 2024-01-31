4d6237fab524ba7c983ebee75782fdf9f770d49e
Marcus Vinicius Martins Marco <marcvini1105@gmail.com>
2024-01-31 12:29:09 -0300

    Created the About us Page Layout

--- vite-project/src/pages/ContactPage.tsx
+++ vite-project/src/pages/ContactPage.tsx
@@ -3,8 +3,8 @@ import FormContact from "../components/FormContact";
 export default function ContactPage() {
 	return (
 		<main className="flex-1 bg-gray-600 overflow-auto sm:flex justify-center">
-			<div className="flex flex-col justify-center items-center gap-5 rounded-tl-md rounded-tr-3xl rounded-bl-3xl rounded-br-md py-10 bg-black sm:w-2/3 my-14">
-				<h1 className="text-green-600 text-4xl sm:text-6xl font-extrabold">
+			<div className="flex flex-col justify-center items-center gap-1 rounded-tl-md rounded-tr-3xl rounded-bl-3xl rounded-br-md bg-black max-sm:py-5 sm:w-2/3 my-14">
+				<h1 className="text-green-600 text-4xl lg:text-5xl font-extrabold">
 					Contact us
 				</h1>
 
